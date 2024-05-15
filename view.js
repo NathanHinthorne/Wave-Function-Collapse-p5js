e82e2556515da7b98e026e4a8bdf3372b8b8bf8a
Nathan Hinthorne <nathanrh856@gmail.com>
2024-05-14 23:17:00 -0700

    finished tile variants display. Made analyze button. Enhanced other buttons

--- view.js
+++ view.js
@@ -1,4 +1,4 @@
-let playButton, pauseButton, resetButton, dimInput, fileInput;
+let analyzeButton, playButton, pauseButton, resetButton, dimInput, fileInput;
 
 function setupView() {
     // --- CANVAS ---
@@ -24,22 +24,6 @@ function setupView() {
     fileInput.position(100, 480);
     fileInput.style('display', 'block'); 
     fileInput.attribute('accept', '.png');
-
-    function handleFile(file) {
-        print("user submitted file: ", file.name);
-
-        // TODO check if file is an image
-        if (file.type === 'image') {
-            inputImage = loadImage(file.data, () => {
-                parseImage();
-                if (select('#example-text')) {
-                    select('#example-text').remove();
-                }
-            });
-        } else {
-            inputImage = null;
-        }
-    }
 
     // Create tile pixel size input box
     tileSizeInput = createInput('');
@@ -68,46 +52,56 @@ function setupView() {
 
     // --- OUTPUT IMAGE PARAMETERS ---
 
-    // Create play button
+    // Create an analyze button
     const buttonX = 500;
-    const startButtonY = 100;
+    const firstButton = 100;
+    analyzeButton = createButton('Analyze');
+    analyzeButton.position(buttonX, firstButton);
+    analyzeButton.mousePressed(analyze);
+    analyzeButton.style('width', '120px');
+
+    // Create play button
     playButton = createButton('');
-    playButton.position(buttonX, startButtonY);
+    playButton.position(buttonX, firstButton + 60);
     playButton.mousePressed(playAnimation);
     playButton.elt.innerHTML = '<i class="fas fa-play"></i>'; // Place icon inside the button
-    
+    playButton.disabled = true; // Disable the button until the image is analyzed
+    playButton.class('grayed-out');
+
     // Create pause button
     // pauseButton = createButton('');
-    // pauseButton.position(buttonX, startButtonY + 60);
+    // pauseButton.position(buttonX, startButtonY + 120);
     // pauseButton.mousePressed(pauseAnimation);   
     // pauseButton.elt.innerHTML = '<i class="fas fa-pause"></i>'; // Place icon inside the button
     
     // Create reset button
     resetButton = createButton('');
-    resetButton.position(buttonX, startButtonY + 120);
+    resetButton.position(buttonX, firstButton + 180);
     resetButton.mousePressed(resetAnimation);  
     resetButton.elt.innerHTML = '<i class="fas fa-undo"></i>'; // Place icon inside the button
+    resetButton.disabled = true; // Disable the button until the image is analyzed
+    resetButton.class('grayed-out');
 
-    // Create dimension input box
-    dimInput = createInput('');
-    dimInput.position(865, 540);
-    dimInput.style('width', '120px');
-    dimInput.input(() => {
-        updateSliderFromInput(dimSlider, dimInput);
-        updateDim();
-    });
-    dimInput.changed(validateInput)
-    dimInput.attribute('placeholder', 'Dimensions'); // Set placeholder text
+    // // Create dimension input box
+    // dimInput = createInput('');
+    // dimInput.position(865, 540);
+    // dimInput.style('width', '120px');
+    // dimInput.input(() => {
+    //     updateSliderFromInput(dimSlider, dimInput);
+    //     updateDim();
+    // });
+    // dimInput.changed(validateInput)
+    // dimInput.attribute('placeholder', 'Dimensions'); // Set placeholder text
 
-    // Create dimension slider
-    dimSlider = createSlider(0, 100, 0);
-    dimSlider.position(850, 600);
-    dimSlider.style('width', '145px');
-    dimSlider.input(() => {
-        updateInputFromSlider(dimInput, dimSlider);
-        updateDim();
-    });
-    dimSlider.hide();
+    // // Create dimension slider
+    // dimSlider = createSlider(0, 100, 0);
+    // dimSlider.position(850, 600);
+    // dimSlider.style('width', '145px');
+    // dimSlider.input(() => {
+    //     updateInputFromSlider(dimInput, dimSlider);
+    //     updateDim();
+    // });
+    // dimSlider.hide();
 
     // Create a loading bar for the progress of the algorithm
     // loadingBar = createDiv('');
@@ -150,10 +144,25 @@ function setupView() {
     githubLink.class('github-link');
     githubLink.elt.innerHTML += '<i class="fab fa-github"></i>'; // Place icon inside the button
     githubLink.style('width', '145px');
+
+    // Create a link to my linked in
+    // const linkedInLink = createA('https://www.linkedin.com/in/nathan-hinthorne/', 'LinkedIn ');
+    // linkedInLink.position(180, 655);
+    // linkedInLink.class('linkedin-link');
+    // linkedInLink.elt.innerHTML += '<i class="fab fa-linkedin"></i>'; // Place icon inside the button
+    // linkedInLink.style('width', '145px');
+    
+}
+
+function analyze() {
+    analyzeTiles();
+    imageIsAnalyzed = true;
+    enableButtons(true);
 }
 
 function playAnimation() {
-    // 
+    // startOver();
+    // populateOutputGrid();
 }
 
 function pauseAnimation() {
@@ -173,16 +182,20 @@ function updateDim() {
 }
 
 function updateTileSize() {
+
     if (!isNaN(tileSizeInput.value())) {
-        if (tileSizeInput.value() >= 10) {
+        if (tileSizeInput.value() >= 10 && tileSizeInput.value() <= 100){
             tilePixelSize = parseFloat(tileSizeInput.value());
+
             parseImage();
+            imageIsAnalyzed = false;
+            enableButtons(false);
+            redraw();
 
             const error = select('#tile-size-error');
             error.remove();
         } else {
-            // show red text saying "Tile size must be at least 10px"            
-            let errorText = createP('Tile size must be at least 10px');
+            let errorText = createP('Tile size must be between 10 and 100 pixels.');
             const error = select('#tile-size-error');
             if (error) {
                 error.remove();
@@ -219,4 +232,101 @@ function updateSliderFromInput(slider, input) {
 
 function updateInputFromSlider(input, slider) {
     input.value(slider.value());
+}
+
+
+function handleFile(file) {
+    print("user submitted file: ", file.name);
+    imageIsAnalyzed = false;
+    enableButtons(false);
+
+    // TODO check if file is an image
+    if (file.type === 'image') {
+        inputImage = loadImage(file.data, () => {
+            parseImage();
+            redraw();
+
+
+            if (select('#example-text')) {
+                select('#example-text').remove();
+            }
+        });
+    } else {
+        inputImage = null;
+    }
+}
+
+function enableButtons(isEnabled) {
+    if (isEnabled) {
+        playButton.disabled = false;
+        resetButton.disabled = false;
+        playButton.removeClass('grayed-out');
+        resetButton.removeClass('grayed-out');
+    } else {
+        playButton.disabled = true;
+        resetButton.disabled = true;
+        playButton.class('grayed-out');
+        resetButton.class('grayed-out');
+    }
+}
+
+
+function displayTileVariants(cardX, cardY, cardWidth, cardHeight) {
+    // put a light gray background behind the tile variants
+    fill(230);
+    noStroke();
+    rect(cardX, cardY, cardWidth, cardHeight, 2);
+
+    fill(0);
+    textSize(14);
+    text("Tile Variants", cardX + 10, cardY + 20);
+
+    // show the image associated with each tile variant, along with its index on the canvas
+    const newTileDisplaySize = 30;
+    const margin = 10;
+    const spacing = newTileDisplaySize / 10 + 1;
+    const rowSpacing = 15;
+
+    // Calculate rowSize based on the cardWidth
+    const rowSize = Math.floor((cardWidth - 2 * margin) / (newTileDisplaySize + spacing));
+
+    let tileYPos = cardY + 30; // Adjust initial Y position to accommodate the title
+    let tileXPos = cardX;
+
+    const maxRows = Math.floor((cardHeight - 2 * margin) / (newTileDisplaySize + spacing + rowSpacing));
+
+    for (let i = 0; i < maxRows * rowSize && i < tileVariants.length; i++) {
+        const tile = tileVariants[i];
+
+        // Calculate the current row and column
+        const row = Math.floor(i / rowSize);
+        const col = i % rowSize;
+
+        // Calculate the x and y position based on the row and column
+        tileXPos = cardX + col * (newTileDisplaySize + spacing) + margin;
+        tileYPos = cardY + 30 + row * (newTileDisplaySize + spacing + rowSpacing);
+
+        image(tile.img, tileXPos, tileYPos, newTileDisplaySize, newTileDisplaySize);
+        fill(0);
+        textSize(8);
+        text(i, tileXPos, tileYPos + newTileDisplaySize + 10);
+
+        // Draw black lines around the tile
+        stroke(0);
+        strokeWeight(1);
+        noFill();
+        rect(tileXPos, tileYPos, newTileDisplaySize, newTileDisplaySize);
+
+        // Reset fill, stroke, and strokeWeight for the next iteration
+        fill(255);
+        stroke(255);
+        strokeWeight(0);
+    }
+
+    // show ... if there are more than maxRows rows
+    if (tileVariants.length > maxRows * rowSize) {
+        fill(0);
+        textSize(18);
+        text("...", cardX + 10, cardY + cardHeight - 5);
+    }
 }
